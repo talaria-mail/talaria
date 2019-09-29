16a6f221aeb6417af371890d265b9abc57442bf3
Nathan Smith <nathan@nfsmith.ca>
2019-09-28 17:21:21 -0700

    Don't start up IMAP server as its broken

--- cmd/talaria/main.go
+++ cmd/talaria/main.go
@@ -3,16 +3,12 @@ package main
 import (
 	"fmt"
 
-	"code.nfsmith.ca/talaria/pkg/imap"
 	"code.nfsmith.ca/talaria/pkg/submission"
 )
 
 func main() {
 	var sub submission.Server
 	sub.Addr = "localhost:2525"
-
-	var imapServer imap.Server
-	imapServer = "localhost:8143"
 
 	fmt.Println(sub.ListenAndServe())
 }
