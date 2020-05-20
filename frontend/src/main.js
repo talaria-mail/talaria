adcdf2f57393d12b1429cb653f526460e0d28b4c
Nathan Smith <nathan@nfsmith.ca>
2020-05-20 16:51:03 -0700

    Start using vuex for state

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,13 +2,15 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 
 import router from "./router/router";
-import App from './App.vue'
+import store from "./store";
+import App from './App.vue';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 new Vue({
   router,
+  store,
   render: h => h(App),
 }).$mount('#app')
