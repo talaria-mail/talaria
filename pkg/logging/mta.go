87bcf6974bdbaae2eb76bbedd6c34096bd02c1d5
Nathan Smith <nathan@nfsmith.ca>
2021-01-31 14:44:35 -0800

    Comment logging middleware

--- pkg/logging/mta.go
+++ pkg/logging/mta.go
@@ -8,6 +8,7 @@ import (
 	"code.nfsmith.ca/nsmith/talaria/pkg/talaria"
 )
 
+// MTAMiddleware logs mta.Sender requests
 func MTAMiddleware(next mta.Sender) mta.Sender {
 	return mta.SenderFunc(func(from mail.Address, to mail.Address, msg talaria.Message) error {
 		log.Printf("mta: from=%s to=%s subj=%s", from.Address, to.Address, msg.Header.Get("Subject"))
--- pkg/logging/pubsub.go
+++ pkg/logging/pubsub.go
@@ -8,10 +8,12 @@ import (
 	"code.nfsmith.ca/nsmith/talaria/pkg/talaria"
 )
 
+// PubSubMiddleware logs pubsub events
 type PubSubMiddleware struct {
 	Next pubsub.PubSub
 }
 
+// Publish logs event and then publishes to Next
 func (ps *PubSubMiddleware) Publish(ctx context.Context, evt interface{}) error {
 	switch msg := evt.(type) {
 	case *talaria.OutboundMessage:
@@ -22,6 +24,7 @@ func (ps *PubSubMiddleware) Publish(ctx context.Context, evt interface{}) error 
 	return ps.Next.Publish(ctx, evt)
 }
 
+// Subscribe logs event and then subscribes to Next
 func (ps *PubSubMiddleware) Subscribe(ctx context.Context) (<-chan interface{}, <-chan error) {
 	log.Println("pubsub: method=Subscribe")
 	return ps.Next.Subscribe(ctx)
