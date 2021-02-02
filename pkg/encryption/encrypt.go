a1639143dfdaf9f1862b3fceb125dafbe83688fc
Nathan Smith <nathan@nfsmith.ca>
2021-02-01 22:41:21 -0800

    wip curve25519 encryption

---
+++ pkg/encryption/encrypt.go
@@ -0,0 +1,48 @@
+package encryption
+
+import (
+	"crypto/rand"
+	"errors"
+	"io"
+
+	"golang.org/x/crypto/chacha20poly1305"
+	"golang.org/x/crypto/curve25519"
+)
+
+func encrypt(pub [32]byte, plain []byte) (encrypted []byte, err error) {
+	// Generate a random curve25519 key pair
+	var epub, epriv [32]byte
+	_, err = io.ReadFull(rand.Reader, epriv[:])
+	if err != nil {
+		return
+	}
+	curve25519.ScalarBaseMult(&epub, &epriv)
+
+	// ECDH to create a shared secret
+	var shared [32]byte
+	curve25519.ScalarMult(&shared, &epriv, &pub)
+
+	// chacha20poly1305 aead encrypt plain with shared secret
+	aead, err := chacha20poly1305.NewX(shared[:])
+	if err != nil {
+		return nil, err
+	}
+
+	nonce := make([]byte, aead.NonceSize(), aead.NonceSize()+len(plain)+aead.Overhead())
+	_, err = io.ReadFull(rand.Reader, nonce)
+	if err != nil {
+		return nil, err
+	}
+
+	encrypted = aead.Seal(nonce, nonce, plain, nil)
+
+	// Append ephemeral public key to message
+	encrypted = append(encrypted, epub[:]...)
+	return
+}
+
+func decrypt(priv [32]byte, encrypted []byte) (plain []byte, err error) {
+	// Split ephemeral key out
+
+	return nil, errors.New("not implemented")
+}
