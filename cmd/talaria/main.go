64dc430017bf0b1f57a965fdbfe28db6ee2d79f9
Nathan Smith <nathan@nfsmith.ca>
2021-03-06 21:45:11 -0800

    Viper configuration

--- cmd/talaria/main.go
+++ cmd/talaria/main.go
@@ -4,8 +4,9 @@ import "github.com/spf13/cobra"
 
 func main() {
 	cmd := &cobra.Command{
-		Use:   "talaria",
-		Short: "Email simplified",
+		Use:              "talaria",
+		Short:            "Email simplified",
+		PersistentPreRun: Configure,
 	}
 
 	cmd.AddCommand(NewServeCmd())
